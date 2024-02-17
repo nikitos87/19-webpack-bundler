21daddf97b002c827405a4628338e915e1abeca8
Nikita Ambalov <js.python.st@gmail.com>
2024-02-17 21:23:22 +0300

    commit exercise #4

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+import "./index.css";
+import JS_IMAGE from "./assets/js.png";
+
 console.log("Hello, World!");
+
+const body = document.querySelector("body");
+
+const someHeader = document.createElement("h2");
+someHeader.textContent = "I love JavaScript";
+const jsImage = document.createElement("img");
+jsImage.setAttribute("src", JS_IMAGE);
+
+body.append(someHeader);
+body.append(jsImage);
